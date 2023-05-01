497208553158e8afe1cdb856d30ddbef4c9559d0
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-05-01 22:52:49 +0530

    file upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import FileUploadScreen from "./screens/FileUploadScreen";
+import { getSingleFiles, getMultipleFiles } from "./data/api";
 
 function App() {
+  const [singleFiles, setSingleFiles] = useState([]);
+  const [multipleFiles, setMultipleFiles] = useState([]);
+
+  const getSingleFileslist = async () => {
+    try {
+      const fileslist = await getSingleFiles();
+      console.log("fileslist", fileslist);
+      setSingleFiles(fileslist.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const getMultipleFilesList = async () => {
+    try {
+      const fileslist = await getMultipleFiles();
+      setMultipleFiles(fileslist.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getSingleFileslist();
+    getMultipleFilesList();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h3 className="text-danger font-weight-bolder border-bottom text-center">
+          Single & Multiple File Upload Using MERN Stack{" "}
+        </h3>
+        <FileUploadScreen
+          getsingle={() => getSingleFileslist()}
+          getMultiple={() => getMultipleFilesList()}
+        />
+      </div>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col-6">
+            <h4 className="text-success font-weight-bold">Single Files List</h4>
+            <div className="row">
+              {singleFiles &&
+                singleFiles.map((item) => (
+                  <div className="col-6">
+                    <div className="card mb-2 border-0 p-0">
+                      <img
+                        src={`http://localhost:8080/${item.filePath}`}
+                        height="200"
+                        className="card-img-top img-responsive"
+                        alt="img"
+                      />
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="col-6">
+            <h4 className="text-success font-weight-bold">
+              Multiple Files List
+            </h4>
+            {multipleFiles &&
+              multipleFiles.map((element, index) => (
+                <div key={element._id}>
+                  <h6 className="text-danger font-weight-bold">
+                    {element.title}
+                  </h6>
+                  <div className="row">
+                    {element.files.map((file, index) => (
+                      <div className="col-6">
+                        <div className="card mb-2 border-0 p-0">
+                          <img
+                            src={`http://localhost:8080/${file.filePath}`}
+                            height="200"
+                            className="card-img-top img-responsive"
+                            alt="img"
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
